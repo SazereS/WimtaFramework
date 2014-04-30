6ef1849cc87f2445354e44fc7d9ee41bb28a6994
SazereS <saze12321@gmail.com>
2014-04-30 15:07:29 +0400

    Багфикс с элементом Password
    ^ Исправлена ошибка с названием класса

--- source/library/module/BootstrapForm/Element/Password.php
+++ source/library/module/BootstrapForm/Element/Password.php
@@ -2,7 +2,7 @@
 
 namespace Library\Module\BootstrapForm\Element;
 
-class Email extends \Library\Module\BootstrapForm\Element\Text
+class Password extends \Library\Module\BootstrapForm\Element\Text
 {
 
     protected $_template = '<input %1$s value="%2$s" type="password" />';
