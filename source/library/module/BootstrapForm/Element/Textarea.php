757b2cc22856c6dfb8b39628b09c05606abe868f
SazereS <saze12321@gmail.com>
2014-06-12 22:50:58 +0400

    Отформатировал

--- source/library/module/BootstrapForm/Element/Textarea.php
+++ source/library/module/BootstrapForm/Element/Textarea.php
@@ -5,14 +5,14 @@ namespace Library\Module\BootstrapForm\Element;
 class Textarea extends \Library\Module\BootstrapForm\Element
 {
 
-protected $_template = '<textarea %1$s>%2$s</textarea>';
-protected $_value_protect = false;
-protected $_classes = 'form-control';
-
-public function __construct($name)
-{
-parent::__construct($name);
-}
+    protected $_template = '<textarea %1$s>%2$s</textarea>';
+    protected $_value_protect = false;
+    protected $_classes = 'form-control';
+    
+    public function __construct($name)
+    {
+        parent::__construct($name);
+    }
 
 }
 
