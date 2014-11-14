0d80da4920284bdd3b15ae563233b25f65977a5d
SazereS <saze12321@gmail.com>
2014-11-14 13:54:28 +0300

    Update reg.sh

--- reg.sh
+++ reg.sh
@@ -2,5 +2,7 @@
 # Добавление текущей директории к переменной PATH
 export PATH=$PATH:$PWD
 export Wimta_Path=$PWD
+# Делаю консольный помошник исполняемым
+chmod +x wimta.sh
 echo "Finished! You just successfully registered Wimta's path! Press any key to exit..."
 read -n 1 -s
